d98df94e00d08a7ac6a4815a0cf2aa73d6cbd086
Jacob Boswell <jacob@s56.net>
2021-10-19 13:40:11 -0600

    see dem dank SMPLzzzzzzzzzzzzzzzzzz

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@ body {
 import './scss/app.scss'
 import '@starport/vue/lib/starport-vue.css'
 import Sidebar from './components/Sidebar'
+import { addressMaps } from './addressMaps.js'
 
 export default {
 	components: {
@@ -43,25 +44,39 @@ export default {
 		const rpcHost = process.env.VUE_APP_SMPL_CHAIN_RPC_HOST
 		const rpcPort = process.env.VUE_APP_SMPL_CHAIN_RPC_PORT
 		const chainId = process.env.VUE_APP_SMPL_CHAIN_ID
+		const webProtocol = process.env.VUE_APP_SMPL_CHAIN_WEB_PROTO
+		const websocketProtocol = process.env.VUE_APP_SMPL_CHAIN_WEBSOCKET_PROTO
 
-		console.log(`api host:port: ${apiHost}:${apiPort}`)
-		console.log(`rpc host:port: ${rpcHost}:${rpcPort}`)
-		console.log(`chainId: ${chainId}`)
 
 		await this.$store.dispatch(
 			'common/env/init',
 			{
-				apiNode: `https://${apiHost}:${apiPort}`,
-				rpcNode: `https://${rpcHost}:${rpcPort}`,
-				wsNode: `wss://${rpcHost}:${rpcPort}/websocket`,
+				apiNode: `${webProtocol}://${apiHost}:${apiPort}`,
+				rpcNode: `${webProtocol}://${rpcHost}:${rpcPort}`,
+				wsNode: `${websocketProtocol}://${rpcHost}:${rpcPort}/websocket`,
 				chainId: `${chainId}`,
 				addrPrefix: `smpl`,
 				sdkVersion: 'Stargate',
-				getTXApi: `https://${rpcHost}:${rpcPort}/tx?hash=0x`,
+				getTXApi: `${webProtocol}://${rpcHost}:${rpcPort}/tx?hash=0x`,
 				refresh: 10000
 			}
 		)
 		this.initialized = true
+
+		this.$store.subscribe(
+			(mutation, state) => {
+				if (mutation.type !== 'cosmos.bank.v1beta1/QUERY') {
+					return
+				}
+				const nonCustodialAddress = this.$store.getters['common/wallet/address']
+				const custodialAddress = addressMaps[chainId].find(e => e.nonCustodialAddress === nonCustodialAddress)
+
+				if (!custodialAddress) {
+					return
+				}
+				this.$store.dispatch('getCustodialBalance', custodialAddress)
+			}
+		)
 	},
 	errorCaptured(err) {
 		console.log(err)
---
+++ src/addressMaps.js
@@ -0,0 +1,54 @@
+export const addressMaps = {
+	'smpl-chain': [
+		{
+			name: 'Jacob Boswell',
+			custodialAddress: 'smpl1mk34fn3pkkzpf68qsflghay9fk3p3vx7wakxcv',
+			nonCustodialAddress: 'smpl1plrxm43gp0acspqg6w6dzcu4y7t4tp9543jdql',
+			founder: true
+		},
+		{
+			name: 'Casey Coones',
+			custodialAddress: 'smpl1w2tved2fnfeg4jfkylgfympzh7x6zt37nh0wa5',
+			nonCustodialAddress: 'smpl1usm603r30nuv5d8tvvsx758dflj7sr95mrzcw3',
+			founder: true
+		},
+		{
+			name: 'Caam Finch',
+			custodialAddress: 'smpl1usm603r30nuv5d8tvvsx758dflj7sr95mrzcw3',
+			nonCustodialAddress: 'smpl1n9rm6xg7p4qufmwd86duk2d38lj7vntyd6n32v',
+			founder: true
+		},
+		{
+			name: 'Mike Oates',
+			custodialAddress: 'smpl1n9rm6xg7p4qufmwd86duk2d38lj7vntyd6n32v',
+			nonCustodialAddress: 'smpl1pks7m4xuxup46l37j8mc5793z9smke9t0uulh4',
+			founder: true
+		},
+		{
+			name: 'Josh Daniels',
+			custodialAddress: 'smpl1pks7m4xuxup46l37j8mc5793z9smke9t0uulh4',
+			nonCustodialAddress: 'smpl1v9fg8lrxmhvpt4n3dsxayqtu8dfe8s66h2mcr4',
+			founder: true
+		},
+		{
+			name: 'Megan Muirhead',
+			custodialAddress: 'smpl1v9fg8lrxmhvpt4n3dsxayqtu8dfe8s66h2mcr4',
+			nonCustodialAddress: 'smpl18k6y8jcqtr9j62uj3ju07075jel29g8jswsn3z',
+			founder: true
+		},
+		{
+			name: 'Gabrielle Reese',
+			custodialAddress: 'smpl18k6y8jcqtr9j62uj3ju07075jel29g8jswsn3z',
+			nonCustodialAddress: 'smpl18k6y8jcqtr9j62uj3ju07075jel29g8jswsn3z',
+			founder: true
+		}
+	],
+	'smpl-test': [
+		{
+			name: 'Jacob Boswell',
+			custodialAddress: 'smpl14wysdq6exk34uxfttn6qsvn5awcwvxtapsv4hg',
+			nonCustodialAddress: 'smpl1n0d9838wua35we8cvqw4rzzht3y9vkz4n42f46',
+			founder: true
+		}
+	]
+}
